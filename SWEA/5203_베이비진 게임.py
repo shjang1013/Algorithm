1302402e505e3416ba832b774e6b0ed31d58c504
shjang1013 <shjang113@gmail.com>
2020-04-13 01:41:13 +0900

    Update SWEA Problem

--- SWEA/5203_베이비진 게임.py
+++ SWEA/5203_베이비진 게임.py
@@ -1,44 +1,48 @@
 # 문제
 # SWEA 5203 - [파이썬 S/W 문제해결 구현 3일차] 탐욕 알고리즘 - 베이비진 게임
 
-# 나의 코드(실패)
-def BabyJin(player, p):
-    if p in player:   # 받은 카드가 기존에 있을 때 triplet 찾기
-        player[p] += 1
-        if player[p] >= 3: # triplet
+# 나의 코드(성공)
+def babyGin(player, card, i):
+    if player[i] >= 3:
+        return True
+    
+    else:
+        if i - 1 in card:
+            if i - 2 in card or i + 1 in card:
+                return True
+        elif i + 1 in card and i + 2 in card:
             return True
 
-    else:  # 받은 카드가 기존에 없을 때 run 찾아보기
-        player[p] = 1
-        if p - 1 in player:
-            if p - 2 in player or p + 1 in player:
-                return True
-            elif p + 1 in player and p + 2 in player:
-                return True
-        return False
-
+    return False
 
 T = int(input())
 
 for tc in range(T):
-    List = list(map(int, input().split()))
+    cards = list(map(int, input().split()))
+    
+    card1 = []
+    card2 = []
+    
+    # count
+    player1 = [0] * 10
+    player2 = [0] * 10
     
     result = 0
     
-    player1 = player2 = []
-    
-    for i in range(len(List)):
-        # 짝수번째 카드는 p1에게 주고 게임이 끝나면
+    for i in range(len(cards)):
         if i % 2 == 0:
-            player1.append(List[i])
-            if BabyJin(player1, List[i]):
+            card1.append(cards[i])
+            player1[cards[i]] += 1
+            if babyGin(player1, card1, cards[i]):
                 result = 1
-            break
-        # 홀수번째 카드는 p2에게 주고 게임이 끝나면
-        if i % 2 == 1:
-            player2.append(List[i])
-            if BabyJin(player2, List[i]):
+                break
+    
+        else:
+            card2.append(cards[i])
+            player2[cards[i]] += 1
+            if babyGin(player2, card2, cards[i]):
                 result = 2
-            break
+                break
 
-    print('#%d %d' %(tc+1, result))
+    print("#%d %d" %(tc+1, result))
+
