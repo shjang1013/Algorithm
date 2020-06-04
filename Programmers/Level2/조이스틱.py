0eaf24309afe529133f28d9aa920841b2316543a
shjang1013 <shjang113@gmail.com>
2020-06-04 23:59:46 +0900

    Update Programmers Problem

--- Programmers/Level2/조이스틱.py
+++ Programmers/Level2/조이스틱.py
@@ -1,11 +1,39 @@
-# 나의 코드
+# 수정한 코드
 def solution(name):
     answer = 0
+    name = list(name)
+    i = 0
+    base = ['A'] * len(name)
     
-    for i in range(len(name)):
-        up = ord(name[i]) - ord('A')
-        down = ord('Z') - ord(name[i]) + 2
-        answer += min(up, down)
+    while True:
+        right = 1
+        left = 1
+        
+        if name[i] != 'A':
+            answer += min(ord(name[i]) - ord('A'), (ord('Z') - ord(name[i]) + 1))
+            name[i] = 'A'
+        
+        if name == base:
+            break
     
-    # 조이스틱 조작 횟수의 최솟값을 리턴 
+        for j in range(1, len(name)):
+            if name[i+j] == 'A': # 오른쪽
+                right += 1
+            else:
+                break
+            
+            if name[i-j] == 'A': # 왼쪽
+                left += 1
+            else:
+                break
+        
+        
+        answer += left if right > left else right
+        i += -left if right > left else right
+                        
     return answer
+
+# 차근차근 한 줄 씩 작성하기
+1. 조건 제대로 확인하기 => 위, 아래 뿐만 아니라 왼쪽, 오른쪽 조이스틱의 움직임 확인하기
+2. 'A'의 개수가 적은 쪽 찾기 => 왼쪽, 오른쪽 따로 나눠 접근하기
+
--- Programmers/Level2/큰_수_만들기.py
+++ Programmers/Level2/큰_수_만들기.py
@@ -12,3 +12,26 @@ def solution(number, k):
     return str(max(answer))
 
 # 수정한 코드
+def solution(number, k):
+    stack = []
+    
+    for (i, num) in enumerate(number):
+        while stack and stack[-1] < num and k > 0:
+            stack.pop()
+            k -= 1
+        
+        if k == 0:
+            stack += number[i:]
+            break
+        
+        stack.append(num)
+    
+    # 다른 코드 참고하여 수정(2번)
+    if k > 0:
+        stack = stack[:-k]
+    
+    return "".join(stack)
+
+# 숙지할 것
+1. enumerate 사용법
+2. solution("7777777",2)의 경우도 생각하기
