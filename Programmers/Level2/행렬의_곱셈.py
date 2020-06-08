3ac7cda72fe4d937122337bb009618d925a74f76
shjang1013 <shjang113@gmail.com>
2020-06-08 23:44:54 +0900

    Add Programmers Problem

---
+++ Programmers/Level2/행렬의_곱셈.py
@@ -0,0 +1,17 @@
+# 나의 코드(실패) - 예외 : [2,2,2,2,2]
+def solution(arr1, arr2):
+    answer = []
+    
+    for i in range(len(arr1)):
+        row = []
+        for j in range(len(arr2[0])):
+            temp = 0
+            # len(arr1[0]) = len(arr2) : 행렬의 성질
+            for k in range(len(arr1[0])):
+                temp += arr1[i][k] * arr2[k][j]
+            row.append(temp)
+        answer.append(row)
+    
+    return answer
+
+# zip을 이용하여 코드를 작성해 볼 것
