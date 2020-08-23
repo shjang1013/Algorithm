41a050a0584a1e4f206c0c4cd99d156e40c10d3b
shjang1013 <shjang113@gmail.com>
2020-08-24 01:03:21 +0900

    Update 1715_카드정렬하기

--- BAEKJOON/정렬/1715_카드 정렬하기.py
+++ BAEKJOON/정렬/1715_카드 정렬하기.py
@@ -1,3 +1,5 @@
+# 정렬된 두 묶음의 숫자 카드가 있을 때 각 묶음의 카드의 수가 A, B라 하면 보통 두 묶음을 합쳐서 하나로 만드는 데 A+B번의 비교를 해야함
+# 최소한 몇 번의 비교가 필요한지를 구하는 프로그램 작성
 import heapq
 import sys
 
@@ -14,9 +16,10 @@ while len(cards) != 1:
     one = heapq.heappop(cards)
     two = heapq.heappop(cards)
     
-    # 카드 묶음 다시 넣기
     result += (one + two)
-    heapq.heappush(cards, result)
+    
+    # 카드 묶음 다시 넣기
+    heapq.heappush(cards, one + two)
 
 print(result)
 
