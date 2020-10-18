8da3a4ab9edc23ef9ab44958d69372a619b5c07c
shjang1013 <shjang113@gmail.com>
2020-10-18 23:33:52 +0900

    Add BOJ Problem

--- BAEKJOON/백트래킹/15655_N과 M(6).py
+++ BAEKJOON/백트래킹/15655_N과 M(6).py
@@ -1,26 +1,28 @@
-# 1부터 N까지 자연수 중에서 중복 없이 M개를 고른 수열 
+# 1부터 N까지 자연수 중에서 중복 없이 M개를 고른 수열
+# 오름차순
+
 N, M = map(int, input().split())
 
 List = sorted(list(map(int, input().split())))
 
-visited = [0] * N
+visited = [0] * (N + 1)
 
 array = [0] * M
 
-def go(i):
+def go(i, start):
     if i == M:
         for j in range(M):
             print(array[j], end=' ')
         print()
         return
 
-    for j in range(N):
+    for j in range(start, N + 1):
         if visited[j]:
             continue
         
         visited[j] = 1
-        array[i] = List[j]
-        go(i+1)
+        array[i] = List[j-1]
+        go(i + 1, j + 1)
         visited[j] = 0
 
-go(0)
+go(0, 1)
